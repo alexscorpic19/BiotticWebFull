a4f74ad869f8b341d0b6d613c96b2256f9f2924f
alexscorpic19 <alexscorpic19@gmail.com>
2025-09-30 21:44:55 -0500

    fix(frontend): se modifico ProductCard.js

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -11,7 +11,7 @@ const ProductCard = ({ product }) => {
   return (
     <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden transform transition duration-300 hover:scale-105 dark:shadow-lg dark:shadow-gray-900/50">
       <Link to={`/products/${product._id}`}>
-        <img loading="lazy" src={product.images[0]} alt={product.name} className="w-full h-48 object-cover" />
+        <img loading="lazy" src={`${BASE_URL.replace('/api', '')}${product.images[0]}`} alt={product.name} className="w-full h-48 object-cover" />
       </Link>
       <div className="p-4">
         <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200 mb-1">{product.name}</h3>
